52bcb5d2eeb4b38a9b78fc8eb96d58e4c6b5d352
UmarShabbirDev <umarshabbir957@gmail.com>
2025-03-27 18:01:17 +0100

    Functions Called back in main

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 ﻿from fastapi import FastAPI, UploadFile, File, Form
-
+from rag_utils import process_document,process_url,query_pinecone
 
 app = FastAPI
 
@@ -7,14 +7,15 @@ app = FastAPI
 @app.post("/upload-doc/")
 async def upload_doc(file: UploadFile = File(...), openai_key: str = Form(...)):
     file_bytes = await file.read()
-    return {"message": "Document Uploaded Successfully"}
+    result = process_document(file.filename,file_bytes,openai_key)
+    return {"message": "Document Uploaded Successfully", "result":result}
     
 @app.post("/upload-url/")
 async def upload_url(url: str = Form(...), openai_key: str = Form(...)):
-    return {"message": "Url Processed"}
+    result = process_url(url,openai_key)
+    return {"message": "Url Processed", "result":result}
 
 @app.get("/get-answer/")
 async def get_answer(query: str, openai_key:str):
-    return {"answer"}
-    
-    
+    answer = query_pinecone(query,openai_key)
+    return {"answer":answer}
