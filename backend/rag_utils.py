6c92cfa08f3a3481dfd2a0dc3107570bb4735bba
UmarShabbirDev <umarshabbir957@gmail.com>
2025-03-27 16:39:32 +0100

    Embedding code Added

--- backend/rag_utils.py
+++ backend/rag_utils.py
@@ -2,6 +2,8 @@ import os.path
 from langchain_community.document_loaders import PyPDFLoader,TextLoader,Docx2txtLoader, UnstructuredURLLoader
 import tempfile
 
+from langchain.text_splitter import RecursiveCharacterTextSplitter 
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 def process_document(filename:str, file_bytes:bytes):
     with tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splitext(filename)[1]) as tmp:
@@ -22,3 +24,13 @@ def process_document(filename:str, file_bytes:bytes):
 def process_url(url:str):
     loader = UnstructuredURLLoader(urls=[url])
     docs = loader.load()
+    
+def embed_and_store(docs):
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=800,
+        chunk_overlap=150,
+        separators=["\n\n","\n",".","!","?"," ",""]
+    )
+    chunks = splitter.split_documents(docs)
+    
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
